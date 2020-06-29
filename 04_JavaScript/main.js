89e3a240a6a5b1429b8a8c2f89d701d91a604634
Your Name <you@example.com>
2020-06-29 14:10:39 -0500

    intro js y arreglos

---
+++ 04_JavaScript/main.js
@@ -0,0 +1,86 @@
+// probamos que funciona nuestro código
+
+// alert("Hola mundo en JS!");
+
+// Variables
+// var numero = 10564865155.5487;
+// var string = "soy un texto";
+// var boolean = true;
+// var array = [12, 25, "Osvaldo", "Ricardo"];
+// var object = {
+//     nombre: "Osvaldo",
+//     edad: 21,
+//     profesion: "frontend developer"
+// }
+
+// console.log(numero);
+// console.log(object);
+// console.log(string);
+
+// numero = 10;
+// console.log(numero);
+
+// var nombre = "Osvaldo";
+// var edad = 21;
+// var profesion = "frontend";
+
+// var desarrollador =
+//   "Hola, mi nombre es " + nombre + " tengo " + edad + " y soy " + profesion;
+
+// console.log(desarrollador);
+
+// operaciones aritmeticas
+
+// suma "+"
+// resta "-"
+// multiplicación "*"
+// división "/"
+// módulo "%"
+
+// var num1 = 26;
+// var num2 = 87;
+
+// var suma = num1 + num2;
+// var resta = num2 - num1;
+// var modulo = num1 % num2;
+
+// console.log(suma);
+// console.log(resta);
+// console.log(modulo);
+
+//  challenge
+
+// Van a imprimir en consola a través de variables su nombre, su edad, su numero telefónico
+// por separado y luego en una línea todo junto
+
+// var err1 = "4";
+// var err2 = "8";
+
+// var sumaerronea = err1 + err2;
+
+// console.log(sumaerronea);
+
+// console.log(typeof err1);
+
+// var errCorregido1 = Number(err1);
+// var errCorregido2 = Number(err2);
+
+// console.log(typeof errCorregido1);
+
+// var sumaCorrecta = errCorregido1 + errCorregido2;
+
+// console.log(sumaCorrecta);
+
+// preguntandole al usuario
+
+// var userName = prompt("¿Cuál es tu nombre?");
+// var animo = prompt("¿Cómo estás?");
+// var pizza = prompt("¿La pizza con o sin piña?");
+
+// console.log(userName);
+// console.log(animo);
+// console.log(pizza);
+
+// var respuesta = userName + " " + animo + " " + pizza;
+// console.log(respuesta);
+
