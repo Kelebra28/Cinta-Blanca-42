75607695f0827c0ee9cc50de33cec8c919dbc7a7
Kelebra <ricardokelebra@gmail.com>
2020-06-29 20:25:00 -0500

    Condicionales

---
+++ 06_Condicionales/main.js
@@ -0,0 +1,27 @@
+// alert('DESCARGA YAAAA!')
+
+// if(condicion){
+//     accion si es true
+// } else {
+//     accion si es false
+// }
+
+// < menor que
+// <= manor igual
+// > meyor que
+// >= mayor igual
+// = asigna valor ---> var nombre = 'Ricardo'
+// == compara el caracter
+// === compara indenticamente --> el caracter y su tipo valor
+
+
+
+if ( 10 === '10') {
+    console.log('Es verdad')
+}else {
+    console.log('Es falso')
+}
+
+//  ------------ Challenge ----------------
+// Preguntar al usuario un numero
+// Este numero decir si es  par o es impar
