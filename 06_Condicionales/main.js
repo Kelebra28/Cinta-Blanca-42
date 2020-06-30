7eabcd1bf3924de94a04f5fa4dfe83d5550baf46
Kelebra <ricardokelebra@gmail.com>
2020-06-29 20:56:34 -0500

    Condicionales

--- 06_Condicionales/main.js
+++ 06_Condicionales/main.js
@@ -17,11 +17,28 @@
 
 
 if ( 10 === '10') {
-    console.log('Es verdad')
+    // console.log('Es verdad')
 }else {
-    console.log('Es falso')
+    // console.log('Es falso')
 }
+
+
 
 //  ------------ Challenge ----------------
 // Preguntar al usuario un numero
 // Este numero decir si es  par o es impar
+
+
+var pregunta = prompt('Ingresa un numero')
+
+var numero = Number(pregunta)
+
+if(numero % 2 === 0){
+    console.log(numero + ': es par')
+}else if(numero % 2 === 1){
+    console.log(numero + ': es inpar')
+}else {
+    // console.log('Error al ingresar numeros')
+    console.error('Error al ingresar numeros: ' + numero)
+}
+   
