d488ce2d0d3ae9ad15a4601baf2096a031af742b
Kelebra <ricardokelebra@gmail.com>
2020-06-30 21:04:34 -0500

    ciclo for

---
+++ 07_Ciclos/main.js
@@ -0,0 +1,14 @@
+
+
+// for(inicio; limite ; secuencia ){
+    // accion cada vez que se cumple el ciclo
+// }
+
+for(var i = 0; i <= 10; i++){
+    console.log(i)
+}
+
+
+// empiece 500    termine  0  y la secuecia  10
+// empiece  0  termine  1000  y la secuecia  300
+// empiece  0  termine  -500  y la secuecia   5
