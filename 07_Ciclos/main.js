1d23ae85e8bb1f99502408e7500907805678d991
Kelebra <ricardokelebra@gmail.com>
2020-06-30 21:37:23 -0500

    ciclo for

--- 07_Ciclos/main.js
+++ 07_Ciclos/main.js
@@ -5,10 +5,47 @@
 // }
 
 for(var i = 0; i <= 10; i++){
-    console.log(i)
+    // console.log(i)
 }
 
 
 // empiece 500    termine  0  y la secuecia  10
 // empiece  0  termine  1000  y la secuecia  300
 // empiece  0  termine  -500  y la secuecia   5
+
+for(var e = 500; e >= 0; e-= 10){
+    // console.log(e)
+}
+
+for(var a = 0; a <= 1000; a+= 300){
+    // console.log(a)
+}
+
+for(var x = 0; x >= -500; x-= 5){
+    // console.log(x)
+}
+
+// ---------Challege Bueno FizzBuzz ----------
+
+// ciclo for del 1 al 100 de 1 en 1
+// si el numero es divisble entre 3 ----> Fizz
+// si el numero es divisible entre 5 ----> Buzz
+//Si en numero es divisib le entre 3 y 5 ----> FizzBuzz
+
+// 0
+// 1
+// 2
+// 3 Fizz
+// 4
+// 5 Buzz
+// 6
+// ...
+
+// 15 FizzBuzz
+// 16
+
+//no deberia
+
+// 15 Fizz
+// 15 Buzz
+// 15 Fizzbuzz
