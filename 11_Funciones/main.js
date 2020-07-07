b74e0a2f273d275e9211251e57f5d2d7fee865cf
Kelebra <ricardokelebra@gmail.com>
2020-07-06 19:54:41 -0500

    Fuctions

---
+++ 11_Funciones/main.js
@@ -0,0 +1,62 @@
+// var num1 = 10
+// var num2 = 50
+
+// console.log(num1 + num2)
+
+// function suma (x, y) {
+//     console.log( 'parametro x :'+ x)
+//     console.log( 'parametro y :'+ y)
+//     console.log(x + y)
+// }
+
+// console.log('Afuera de la funcion' + x)
+//   x    y
+// suma(10, 10)
+// suma(80, 100)
+// suma(800, 800)
+
+//VANILLA JS
+var num1 = 10
+// console.log(num1)
+num1 = 'Hola'
+// console.log(num1)
+
+ 
+// var suma = function(x, y) {
+//     console.log( 'parametro x :'+ x)
+//     console.log( 'parametro y :'+ y)
+//     console.log(x + y)
+// }
+// console.log(suma)
+// suma(10, 250)
+// console.log(suma)
+
+
+
+//ECMA5
+let num2 = 50
+// console.log(num2)
+num2 = 'Hola xd'
+// console.log(num2)
+
+
+const num3 = 100
+// console.log(num3)
+
+// num3 = 'Hola xd'
+// console.log(num3)
+
+//arrow function o funciones de flecha
+
+const suma = (x, y) => {
+    console.log('La suma de ' + x + ' mas ' + y + ' es: ' + (x +y))
+}
+
+suma(20, 50)
+
+
+const salu2 = (a, b) => {
+    console.log('Saludos ' + a + ' de ' + b + ' años de edad')
+}
+
+salu2('Ricardo', 22)
