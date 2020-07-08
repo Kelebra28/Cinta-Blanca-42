8052fe91ca83cbf9985c38d1f93901a16e4a3296
Kelebra <ricardokelebra@gmail.com>
2020-07-07 20:16:30 -0500

    DOM

--- 13_DOM/main.js
+++ 13_DOM/main.js
@@ -1,13 +1,25 @@
 // accedemos a la etiqueta por medio de un ID
 let titulo = document.getElementById('titulo')
 let subtitulo = document.getElementById('subtitulo')
+let pregunta = document.getElementById('pregunta')
+let respuesta = document.getElementById('respuesta')
 let boton = document.getElementById('btn')
+
 
 // console.log(titulo)
 // console.log(subtitulo)
 
+// forma antiugua
+const saludar = () => {
+    alert('Hola mundo!')
+}
+
+
+
+
 const cambiar = ()  => {
     
+    respuesta.innerHTML = pregunta.value
     titulo.innerHTML = 'Me cambie desde la terminal'
     subtitulo.innerHTML = 'Me cambie x2'
     
@@ -17,10 +29,6 @@ boton.addEventListener('click', cambiar)
 
 
 
-// forma antiugua
-const saludar = () => {
-    alert('Hola mundo!')
-}
 
 
 
